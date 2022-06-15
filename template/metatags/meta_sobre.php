a72947489409f58e9f7ecfa6a59af481caaafbbe
Cygnus-x1br <jeanmarcel83@gmail.com>
2022-06-15 12:01:10 -0300

    MVC MetaUPD

---
+++ template/metatags/meta_sobre.php
@@ -0,0 +1,3 @@
+<title>Meta Cygnus RH * Sobre nós</title>
+<meta name="description" content="Cygnus Recursos Humanos. Agencia de empregos em Mogi Guaçu. Trabalhamos vagas de emprego em toda a região da Baixa Mogiana (Mogi Guaçu, Mogi Mirim, Itapira, Socorro, Jacutinga, Estiva Gerbi) e ABC Paulista" />
+<link rel="canonical" href="https://cygnusrh.com.br/sobre.php">
--- template/metatags/meta_vagas.php
+++ template/metatags/meta_vagas.php
@@ -1,3 +1,3 @@
-<title>Vagas de emprego: consulte aqui nossas oportunidades</title>
+<title>Meta Vagas de emprego: consulte aqui nossas oportunidades</title>
 <meta name="description" content="Vagas de emprego em Mogi Guaçu, Mogi Mirim, Itapira, Socorro, Jacutinga, Estiva Gerbi e ABC Paulista. Consulte nossas vagas ou envie seu curriculo" />
 <link rel="canonical" href="https://cygnusrh.com.br/vagas.php">
