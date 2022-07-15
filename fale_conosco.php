b6004a02a724ee18e477c505b7fe3c2a349ca666
Cygnus-x1br <jeanmarcel83@gmail.com>
2022-07-15 13:13:05 -0300

    Alteração para o form submit

--- fale_conosco.php
+++ fale_conosco.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
 
 <?php
-include('./php/emailFunction.php');
+// include('./php/emailFunction.php');
 include('./include/head.php');
 ?>
 <title>Cygnus * Fale Conosco</title>
@@ -15,14 +15,15 @@ include('./include/head.php');
 </style>
 </head>
 <?php
-if (isset($_POST['name'])) {
-    if (enviarMensagem($_POST)) {
-        //header("location:sucesso.php");
-        echo '<script>location.replace("sucesso.php");</script>';
-    } else {
-        die("Erro no envio.");
-    }
-}
+// if (isset($_POST['name'])) {
+//     if (enviarMensagem($_POST)) {
+//         header("location:sucesso.php");
+//         echo '<script>location.replace("sucesso.php");</script>';
+//     } else {
+//         die("Erro no envio.");
+//     }
+// }
+// 
 ?>
 <?php
 include('./include/menu.php');
@@ -31,8 +32,19 @@ $page_link = 'https://cygnusrh.com.br/'
 
 
 <div class="container">
+    <form class="form_email" action="https://formsubmit.co/contato@cygnusrh.com.br" method="POST" enctype="multipart/form-data">
+        <h1>Envie seu currículo</h1>
+        <input type="hidden" name="_next" value="<?php echo $page_link ?>sucesso.php" />
+        <input type="hidden" name="_captcha" value="false" />
+        <input type="text" name="name" placeholder="Digite seu nome" required />
+        <input type="text" name="empresa" placeholder="Digite o nome da empresa" required />
+        <input type="email" name="email" placeholder="Digite seu e-mail" required />
+        <textarea placeholder="Digite sua mensagem" name="message" rows="3" cols="40" wrap></textarea>
+        <div class="blind-line"></div>
+        <button type="submit">Enviar</button>
+    </form>
 
-    <form class="form_email" action="fale_conosco.php" method="POST" enctype="multipart/form-data">
+    <!-- <form class="form_email" action="fale_conosco.php" method="POST" enctype="multipart/form-data">
         <h1>Fale conosco</h1>
         <input type="text" name="name" placeholder="Digite seu nome" required />
         <input type="text" name="empresa" placeholder="Digite o nome da empresa" required />
@@ -40,7 +52,7 @@ $page_link = 'https://cygnusrh.com.br/'
         <textarea placeholder="Digite sua mensagem" name="message" rows="11" cols="40" wrap></textarea>
         <div class="blind-line"></div>
         <button type="submit">Enviar</button>
-    </form>
+    </form> -->
 
     <?php
     include('./include/aside.php');
